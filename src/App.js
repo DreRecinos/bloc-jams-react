c588e7246d230233467f63ab241a556459cba406
Dre Recinos <drerecinos.web@gmail.com>
2018-09-21 20:02:26 -0700

    added album component 9.21

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
@@ -5,22 +6,22 @@ import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <header>
-        <nav>
-           <Link to='/'>Landing</Link>
-           <Link to='/library'>Library</Link>
-         </nav>
-         <h1>Bloc Jams</h1>
+          <nav>
+             <Link to='/'>Landing</Link>
+             <Link to='/library'>Library</Link>
+          </nav>
+          <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path= "/album" compnents={Album} />
+          <Route path="/album" component={Album} />
         </main>
       </div>
     );
