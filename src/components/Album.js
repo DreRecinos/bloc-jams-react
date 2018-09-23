94d0179df30954b7479f6eb12e654bd51add24bd
Dre Recinos <drerecinos.web@gmail.com>
2018-09-22 21:37:00 -0700

    component album assignment completed 9.22

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,42 +1,52 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 
-
 class Album extends Component {
   constructor(props) {
-     super(props);
+    super(props);
 
-     const album = albumData.find( album => {
-       return album.slug === this.props.match.params.slug
-     });
+    const album = albumData.find( album => {
+      return album.slug === this.props.match.params.slug
+    });
 
-     this.state = {
-       album: album
-     };
+    this.state = {
+      album: album
+    };
+  }
 
-   }
-
-   render() {
+  render() {
     return (
       <section className="album">
-      <section id="album-info">
-         <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-         <div className="album-details">
-           <h1 id="album-title">{this.state.album.title}</h1>
-           <h2 className="artist">{this.state.album.artist}</h2>
-           <div id="release-info">{this.state.album.releaseInfo}</div>
-         </div>
-       </section>
-       <table id="song-list">
-           <colgroup>
-             <col id="song-number-column" />
-             <col id="song-title-column" />
-             <col id="song-duration-column" />
-           </colgroup>
-           <tbody>
-           </tbody>
-         </table>
-       </section>
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+            {
+              this.state.album.songs.map( (song , index) =>
+              <div key= {index} >
+                <tr>{index +1}</tr>
+                <tr>{song.title}</tr>
+                <tr>{song.duration}</tr>
+              </div>
+          )
+          }
+
+
+          }
+          </tbody>
+        </table>
+      </section>
     );
   }
 }
