590a46daa5ff12828adc1e6b2874c77c2ed53d0c
Dre Recinos <drerecinos.web@gmail.com>
2018-09-22 20:31:38 -0700

    component albums 9.22

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
+
 class Library extends Component {
   constructor(props) {
     super(props);
     this.state = { albums: albumData };
   }
-  render() {
+
+  render () {
     return (
       <section className='library'>
-       {
-         this.state.albums.map( (album, index) =>
-           <Link to={`/album/${album.slug}`} key={index}>
-           <img src={album.albumCover} alt={album.title} />
-             <div>{album.title}</div>
-             <div>{album.artist}</div>
-             <div>{album.songs.length} songs</div>
-           </Link>
-         )
-       }
+        {
+          this.state.albums.map( (album, index) =>
+            <Link to={`/album/${album.slug}`} key={index}>
+              <img src={album.albumCover} alt={album.title} />
+               <div>{album.title}</div>
+               <div>{album.artist}</div>
+               <div>{album.songs.length} songs</div>
+            </Link>
+          )
+        }
       </section>
-     );
-   }
- }
-
- export default Library;
+    );
+  }
+}
+export default Library
